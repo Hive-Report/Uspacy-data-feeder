0cd14429b2e06cf507e1a968409fe67fa94da6ab
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-03-18 23:01:33 +0200

    Update UspaceManager: - Add new methods
    Update app.js:
    - Add extractUSREOU method
    - Add method for converting data
    
    Need to be patched UspaceManager method for creating KEP entity item. I cannot add relationship with company

--- src/UspaceManager.js
+++ src/UspaceManager.js
@@ -25,6 +25,7 @@ class UspaceManager {
 
     async search(param) {
         if (typeof param !== 'string') param = String(param);
+
         const options = {
             method: 'GET',
             url: `https://${process.env.SPACE}.uspacy.ua/search/v1/search`,
@@ -49,6 +50,7 @@ class UspaceManager {
 
     async getEntityFields(entity) {
         if (typeof entity !== 'string') entity = String(entity);
+
         const options = {
             method: 'GET',
             url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/${entity}/fields`
@@ -58,9 +60,10 @@ class UspaceManager {
         return res.data.data;
     }
 
-    async getEntityFieldByCode(entity, code) {
+    async getEntityFieldByItemCode(entity, code) {
         if (typeof entity !== 'string') entity = String(entity);
         if (typeof code !== 'string') code = String(code);
+
         const options = {
             method: 'GET',
             url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/${entity}/fields/${code}`
@@ -73,6 +76,7 @@ class UspaceManager {
     async getEntity(entity, itemId) {
         if (typeof entity !== 'string') entity = String(entity);
         if (typeof itemId !== 'string') itemId = String(itemId);
+
         const options = {
             method: 'GET',
             url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/${entity}/${itemId}`
@@ -86,10 +90,44 @@ class UspaceManager {
         if (typeof entity !== 'string') entity = String(entity);
         if (typeof field !== 'string') field = String(field);
         if (typeof value !== 'string') value = String(value);
+
         const options = {
             method: 'PATCH',
-            url: `https://domain.uspacy.ua/crm/v1/entities/${entity}/${itemId}`,
+            url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/${entity}/${itemId}`,
             data: {[field]: value}
+        };
+        const res = await this.sendRequest(options);
+
+        return res.data;
+    }
+
+    async getKEPsByCompany(companyId) {
+        if (typeof companyId !== 'string') companyId = String(companyId);
+
+        const options = {
+            method: 'GET',
+            url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/companies/${companyId}/related/keps/`
+        };
+        const res = await this.sendRequest(options);
+
+        return res.data.data;
+    }
+
+    async createKEPEntityForCompany(companyId, title, owner, data_formuvannya, data_zakinchennya, na_cloudkey) {
+        if (typeof companyId !== 'string') companyId = String(companyId);
+        if (typeof title !== 'string') title = String(title);
+        if (typeof owner !== 'string') owner = String(owner);
+
+        const options = {
+            method: 'POST',
+            url: `https://${process.env.SPACE}.uspacy.ua/crm/v1/entities/keps`,
+            data: {
+                title: title,
+                owner: owner,
+                data_formuvannya: data_formuvannya,
+                data_zakinchennya: data_zakinchennya,
+                na_cloudkey: na_cloudkey
+            }
         };
         const res = await this.sendRequest(options);
 
--- src/app.js
+++ src/app.js
@@ -3,6 +3,25 @@ import UspaceManager from './UspaceManager.js';
 import UakeyManager from './UakeyManager.js';
 import { startTokenLifecycle } from './tokenManager.js';
 
+function extractUSREOU(html) {
+  const match = html.match(/\b\d{8,}\b/);
+  
+  if (match) {
+      return match[0];
+  } else {
+      console.error("Error: Unable to find correct EDRPOU.");
+      return null;
+  }
+}
+
+function convertToTimestamp(dateStr) {
+  const [day, month, year] = dateStr.split('.').map(Number);
+  
+  const date = new Date(year, month - 1, day);
+
+  return Math.floor(date.getTime() / 1000);
+}
+
 (async () => {
   try {
     console.log('Starting application...');
@@ -27,7 +46,31 @@ import { startTokenLifecycle } from './tokenManager.js';
     // const resEditEntity = await uspacy.editEntityItem('companies', '9987', 'uf_crm_1632905074', 1111);
     // console.log(resEditEntity);
 
-    
+
+
+    // I try to find company ID and update all KEPs linked with the company
+    let USREOU = "";
+    let companyId = "";
+
+    // const testCompanyName = "АСК \"УКРРІЧФЛОТ\"";
+    const testCompanyName = "Тест Тестович";
+
+    companyId = (await uspacy.search(testCompanyName)).companies[0].id;
+    if (!companyId) throw new Error('Company ID was not found!');
+    USREOU = extractUSREOU((await uspacy.getEntity('companies', companyId)).uf_crm_1632905074);
+    if (!USREOU) throw new Error('Company USREOU code was not found!');
+    console.log(`USREOU: ${USREOU}`);
+
+    const paresdKEPs = await parser.fetchUakeyInfo(USREOU);
+    const resAfterCreateKEPEntity = await uspacy.createKEPEntityForCompany(companyId, 'testByProg', '7', convertToTimestamp("18.03.2025"), convertToTimestamp("17.03.2026"), true);
+
+    const KEPsInUspacy = await uspacy.getKEPsByCompany(companyId);
+
+    console.log(KEPsInUspacy);
+    console.log(resAfterCreateKEPEntity); // I cannot add relationship with company
+
+    console.log(await uspacy.getEntity('companies', '9987')); 
+
 
   } catch (err) {
     console.error('❌Error in application:', err);
